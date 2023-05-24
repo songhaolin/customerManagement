0600f57f66fc195b14665943a355591168dc3b8f
song <546987377@qq.com>
2023-05-24 23:02:32 +0800

    注册、登陆。客户信息管理

---
+++ src/main/java/com/mycom/myapp/mapper/CustomerInfoDao.java
@@ -0,0 +1,21 @@
+package com.mycom.myapp.mapper;
+
+import com.mycom.myapp.pojo.CustomerInfo;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerInfoDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(CustomerInfo record);
+
+    CustomerInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKey(CustomerInfo record);
+
+    List<CustomerInfo> getCustomerInfoList();
+    List<CustomerInfo> queryCustomerInfoList(CustomerInfo customerInfo);
+
+}
---
+++ src/main/java/com/mycom/myapp/pojo/CustomerInfo.java
@@ -0,0 +1,173 @@
+package com.mycom.myapp.pojo;
+
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * customer_info
+ * @author 
+ */
+@Data
+public class CustomerInfo implements Serializable {
+    /**
+     * 主键ID
+     */
+    private Integer id;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 性别
+     */
+    private String gender;
+
+    /**
+     * 年龄
+     */
+    private Integer age;
+
+    /**
+     * 电话
+     */
+    private String phone;
+
+    /**
+     * 邮件
+     */
+    private String email;
+
+    /**
+     * 收入
+     */
+    private String income;
+
+    /**
+     * 国家
+     */
+    private String country;
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        CustomerInfo other = (CustomerInfo) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+            && (this.getName() == null ? other.getName() == null : this.getName().equals(other.getName()))
+            && (this.getGender() == null ? other.getGender() == null : this.getGender().equals(other.getGender()))
+            && (this.getAge() == null ? other.getAge() == null : this.getAge().equals(other.getAge()))
+            && (this.getPhone() == null ? other.getPhone() == null : this.getPhone().equals(other.getPhone()))
+            && (this.getEmail() == null ? other.getEmail() == null : this.getEmail().equals(other.getEmail()))
+            && (this.getIncome() == null ? other.getIncome() == null : this.getIncome().equals(other.getIncome()))
+            && (this.getCountry() == null ? other.getCountry() == null : this.getCountry().equals(other.getCountry()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getName() == null) ? 0 : getName().hashCode());
+        result = prime * result + ((getGender() == null) ? 0 : getGender().hashCode());
+        result = prime * result + ((getAge() == null) ? 0 : getAge().hashCode());
+        result = prime * result + ((getPhone() == null) ? 0 : getPhone().hashCode());
+        result = prime * result + ((getEmail() == null) ? 0 : getEmail().hashCode());
+        result = prime * result + ((getIncome() == null) ? 0 : getIncome().hashCode());
+        result = prime * result + ((getCountry() == null) ? 0 : getCountry().hashCode());
+        return result;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIncome() {
+        return income;
+    }
+
+    public void setIncome(String income) {
+        this.income = income;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", gender=").append(gender);
+        sb.append(", age=").append(age);
+        sb.append(", phone=").append(phone);
+        sb.append(", email=").append(email);
+        sb.append(", income=").append(income);
+        sb.append(", country=").append(country);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
